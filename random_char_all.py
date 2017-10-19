c7e10c3f6d801188ed149183eec7be8bdefacbe6
MaMeSal19 <31545216+MaMeSal19@users.noreply.github.com>
2017-10-19 08:59:01 -0700

    Add files via upload

---
+++ random_char_all.py
@@ -0,0 +1,26 @@
+from random import randint
+def randlist(r,usedlist,done):
+	alpha = ["0","1","2","3","4","5","6","7","8","9","0",")","a","A","b","B,","c","C","d","D","e","E","f","F","g","G","h","H","i","I","j","J","k","K","l","L","m","M","n","N","o","O","p","P","q","Q","r","R","s","S","t","T","u","U","v","V","w","W","x","X","y","Y","z","Z","`","[","]","\\",";","'",",",".","/","~","!","@","#","$","%","^","&","*","(",")","_","+","{","}","|",":",'"',"<",">","?"]
+	#print (len(alpha))
+	usedlist[r] = 1
+	c = alpha[r]
+	sum = 0
+	for i in range(len(usedlist)):
+			sum = sum + usedlist[i]
+	#print (sum)
+	if (sum == 93):
+			done = True
+	return c, usedlist, done 
+	 
+def main():
+	count = 0
+	used = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+	done = False 
+	while (done == False): 
+			r = randint(0,93)
+			c,used,done = randlist(r,used,done)
+			print(c,end="")
+			count += 1
+	print (count," random numbers used")
+	
+main()
